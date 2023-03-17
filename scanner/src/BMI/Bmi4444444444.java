d022eede4975e66cfd6d7afbb1cb3181c23afb52
user <user@DESKTOP-2PGRMF4>
2023-03-17 12:07:28 +0900

    커밋

--- scanner/src/BMI/Bmi4444444444.java
+++ scanner/src/BMI/Bmi4444444444.java
@@ -43,6 +43,7 @@ public class Bmi4444444444 {
 				{
 					System.out.println("종료합니다");
 					System.out.println("========================");
+					System.out.println("재수정");
 					//반복문 탈출
 					break;
 				}
